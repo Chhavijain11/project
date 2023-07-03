4c0f0572fc40818eae0bb1818ff5eaa1b52df30c
Chhavijain11 <134512701+Chhavijain11@users.noreply.github.com>
2023-07-03 14:25:10 +0530

    Update Resume_parser.py

--- Resume_parser.py
+++ Resume_parser.py
@@ -7,6 +7,36 @@ import smtplib
 from pyresparser import ResumeParser
 
 
+import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2 import credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+creds = None
+
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+        flow = InstalledAppFlow.from_client_secrets_file(r'C:\Users\Chhavi Jain\OneDrive\Desktop\VS FILES\externship project\client_secret_668185049822-brj2cgur4dnrbvjuuk5c9grre7av8qf8.apps.googleusercontent.com.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+
 
 #creates SMTP session
 s = smtplib.SMTP('smtp.gmail.com', 587)
